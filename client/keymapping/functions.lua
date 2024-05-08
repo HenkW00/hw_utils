206ba6d22f4f27dd522fd55a9695c463b0cc0757
Henk Wietsma <151195189+HenkW00@users.noreply.github.com>
2024-05-09 00:01:23 +0200

    Fix!!!!!!

--- client/keymapping/functions.lua
+++
@@ -1,9 +0,0 @@
----@param name string
----@param desc string
----@param key string
----@param cb function
-CLIENT.RegisterKeyMappingCommand = function(name, desc, key, cb)
-    RegisterCommand(name, cb)
-    RegisterKeyMapping(name, desc, 'keyboard', key)
-    TriggerEvent('chat:addSuggestion', ('/%s'):format(name), desc, {})
-end
